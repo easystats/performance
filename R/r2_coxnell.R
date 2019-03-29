00476788e7c4bb4c277dfef54cb2c4ed51df4601
Daniel <mail@danielluedecke.de>
2019-03-29 11:56:26 +0100

    update namespace

--- R/r2_coxnell.R
+++ R/r2_coxnell.R
@@ -20,8 +20,8 @@ r2_coxnell <- function(model) {
 }
 
 
-
-
+#' @importFrom insight n_obs
+#' @importFrom stats logLik
 .r2_coxnell <- function(model, l_base) {
   l_full <- stats::logLik(model)
   G2 <- -2 * (l_base - l_full)
@@ -35,28 +35,29 @@ r2_coxnell <- function(model) {
 
   r2_coxnell <- 1 - exp(-G2 / n)
 
-  ## TODO should we return a named vector?
-
   names(r2_coxnell) <- "Cox & Snell's R2"
   r2_coxnell
 }
 
 
-
+#' @importFrom insight n_obs
 #' @export
 r2_coxnell.glm <- function(model) {
   r2_coxnell <- (1 - exp((model$dev - model$null) / insight::n_obs(model)))
   names(r2_coxnell) <- "Coxnell's R2"
-  return(r2_coxnell)
+  r2_coxnell
 }
 
 
+#' @importFrom stats logLik update
 #' @export
 r2_coxnell.multinom <- function(model) {
   l_base <- stats::logLik(stats::update(model, ~1, trace = FALSE))
-  return(.r2_coxnell(model, l_base))
+  .r2_coxnell(model, l_base)
 }
 
+
+#' @importFrom stats logLik update
 #' @export
 r2_coxnell.vglm <- function(model) {
   if (!(is.null(model@call$summ) && !identical(model@call$summ, 0))) {
@@ -64,23 +65,29 @@ r2_coxnell.vglm <- function(model) {
   }
 
   l_base <- stats::logLik(stats::update(model, ~1))
-  return(.r2_coxnell(model, l_base))
+  .r2_coxnell(model, l_base)
 }
 
+
+#' @importFrom stats logLik update
 #' @export
 r2_coxnell.clm <- function(model) {
   l_base <- stats::logLik(stats::update(model, ~1))
-  return(.r2_coxnell(model, l_base))
+  .r2_coxnell(model, l_base)
 }
 
+
+#' @importFrom stats logLik update
 #' @export
 r2_coxnell.clm2 <- function(model) {
   l_base <- stats::logLik(stats::update(model, location = ~1, scale = ~1))
-  return(.r2_coxnell(model, l_base))
+  .r2_coxnell(model, l_base)
 }
 
+
+#' @importFrom stats logLik update
 #' @export
 r2_coxnell.polr <- function(model) {
   l_base <- stats::logLik(stats::update(model, ~1))
-  return(.r2_coxnell(model, l_base))
+  .r2_coxnell(model, l_base)
 }
