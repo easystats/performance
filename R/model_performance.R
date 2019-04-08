d785b79855e74b59d90074b428f907228ee8e68e
Daniel Lüdecke <mail@danielluedecke.de>
2019-04-08 17:09:57 +0200

    draft generic model_performance

--- R/model_performance.R
+++ R/model_performance.R
@@ -4,7 +4,8 @@
 #' \itemize{
 #'  \item{\link[=model_performance.lm]{lm}}
 #'  \item{\link[=model_performance.glm]{glm}}
-#'  \item{\link[=model_performance.stanreg]{Bayesian lm}}
+#'  \item{\link[=model_performance.merMod]{mixed models}}
+#'  \item{\link[=model_performance.stanreg]{Bayesian models}}
 #'  }
 #'
 #'
@@ -15,4 +16,24 @@
 #' @export
 model_performance <- function(model, ...) {
   UseMethod("model_performance")
+}
+
+temp_model_performance <- function(...) {
+  objects <- list(...)
+  object_names <- match.call(expand.dots = FALSE)$`...`
+
+  m <- lapply(objects, function(.x) {
+    dat <- model_performance(.x)
+    cbind(data.frame(class = class(.x)[1], stringsAsFactors = FALSE), dat)
+  })
+
+  dfs <- Reduce(function(x, y) merge(x, y, all = TRUE), m)
+
+  cbind(
+    data.frame(
+      name = unlist(lapply(object_names, as.character)),
+      stringsAsFactors = FALSE
+    ),
+    dfs
+  )
 }
