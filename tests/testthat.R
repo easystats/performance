6096e9f81df07812bfd4f7d2817504afff7080be
Daniel Lüdecke <mail@danielluedecke.de>
2019-04-08 17:42:41 +0200

    update description and tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,11 @@
 library(testthat)
 library(performance)
 
+if (length(strsplit(packageDescription("performance")$Version, "\\.")[[1]]) > 3) {
+  Sys.setenv("RunAllinsightTests" = "yes")
+} else {
+  Sys.setenv("RunAllinsightTests" = "no")
+}
+
+
 test_check("performance")
--- tests/testthat/test-model_performance.bayesian.R
+++ tests/testthat/test-model_performance.bayesian.R
@@ -1,48 +1,51 @@
-context("model_performance.bayesian")
+.runThisTest <- Sys.getenv("RunAllinsightTests") == "yes"
 
-test_that("model_performance.stanreg", {
-  set.seed(333)
+if (.runThisTest && Sys.getenv("USER") != "travis") {
+  context("model_performance.bayesian")
 
-  library(rstanarm)
-  library(circus)
-  model <- circus::download_model("stanreg_lm_1")
-  perf <- model_performance(model)
+  test_that("model_performance.stanreg", {
+    set.seed(333)
+    library(rstanarm)
 
-  testthat::expect_equal(perf$R2_Median, 0.751, tolerance = 0.01)
-  testthat::expect_equal(perf$R2_LOO_adjusted, 0.7094, tolerance = 0.01)
-  testthat::expect_equal(perf$ELPD, -83.514, tolerance = 0.01)
+    model <- insight::download_model("stanreg_lm_1")
+    perf <- model_performance(model)
 
-  model <- circus::download_model("stanreg_lm_2")
-  perf <- model_performance(model)
+    testthat::expect_equal(perf$R2_Median, 0.751, tolerance = 0.01)
+    testthat::expect_equal(perf$R2_LOO_adjusted, 0.7094, tolerance = 0.01)
+    testthat::expect_equal(perf$ELPD, -83.514, tolerance = 0.01)
 
-  testthat::expect_equal(perf$R2_Median, 0.6392, tolerance = 0.01)
-  testthat::expect_equal(perf$R2_LOO_adjusted, 0.587247, tolerance = 0.01)
-  testthat::expect_equal(perf$ELPD, -31.622, tolerance = 0.01)
+    model <- insight::download_model("stanreg_lm_2")
+    perf <- model_performance(model)
 
-  model <- circus::download_model("stanreg_lmerMod_1")
-  perf <- model_performance(model)
+    testthat::expect_equal(perf$R2_Median, 0.6392, tolerance = 0.01)
+    testthat::expect_equal(perf$R2_LOO_adjusted, 0.587247, tolerance = 0.01)
+    testthat::expect_equal(perf$ELPD, -31.622, tolerance = 0.01)
 
-  testthat::expect_equal(perf$R2_Median, 0.6392, tolerance = 0.01)
-  testthat::expect_equal(perf$R2_LOO_adjusted, 0.58724, tolerance = 0.01)
-  testthat::expect_equal(perf$ELPD, -31.622, tolerance = 0.01)
-})
+    model <- insightinsight::download_model("stanreg_lmerMod_1")
+    perf <- model_performance(model)
+
+    testthat::expect_equal(perf$R2_Median, 0.6392, tolerance = 0.01)
+    testthat::expect_equal(perf$R2_LOO_adjusted, 0.58724, tolerance = 0.01)
+    testthat::expect_equal(perf$ELPD, -31.622, tolerance = 0.01)
+  })
 
 
-test_that("model_performance.brmsfit", {
-  testthat::skip_on_travis()
-  set.seed(333)
+  test_that("model_performance.brmsfit", {
+    set.seed(333)
 
-  library(brms)
+    library(brms)
 
-  model <- brms::brm(mpg ~ wt + cyl, data = mtcars)
-  perf <- model_performance(model)
-  testthat::expect_equal(perf$R2_Median, 0.826, tolerance = 0.01)
-  testthat::expect_equal(perf$R2_LOO_adjusted, 0.791, tolerance = 0.01)
-  testthat::expect_equal(perf$ELPD, -78.407, tolerance = 0.01)
+    model <- brms::brm(mpg ~ wt + cyl, data = mtcars)
+    perf <- model_performance(model)
+    testthat::expect_equal(perf$R2_Median, 0.826, tolerance = 0.01)
+    testthat::expect_equal(perf$R2_LOO_adjusted, 0.791, tolerance = 0.01)
+    testthat::expect_equal(perf$ELPD, -78.407, tolerance = 0.01)
 
-  model <- brms::brm(Petal.Length ~ Petal.Width + (1 | Species), data = iris)
-  perf <- model_performance(model)
-  testthat::expect_equal(perf$R2_Median, 0.955, tolerance = 0.01)
-  testthat::expect_equal(perf$R2_LOO_adjusted, 0.952, tolerance = 0.01)
-  testthat::expect_equal(perf$ELPD, -70.5, tolerance = 0.01)
-})
+    model <- brms::brm(Petal.Length ~ Petal.Width + (1 | Species), data = iris)
+    perf <- model_performance(model)
+    testthat::expect_equal(perf$R2_Median, 0.955, tolerance = 0.01)
+    testthat::expect_equal(perf$R2_LOO_adjusted, 0.952, tolerance = 0.01)
+    testthat::expect_equal(perf$ELPD, -70.5, tolerance = 0.01)
+  })
+
+}
