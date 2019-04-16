2540c19562ad3c134713cf0858c026f20cf337a7
Daniel <mail@danielluedecke.de>
2019-04-16 09:19:18 +0200

    add tests

---
+++ tests/testthat/test-item_difficulty.R
@@ -0,0 +1,14 @@
+context("item_difficulty")
+
+test_that("item_difficulty", {
+  data(iris)
+  x <- iris[, 1:4]
+  expect_equal(
+    item_difficulty(x),
+    list(
+      item = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
+      difficulty = c("Sepal.Length" = 0.74, "Sepal.Width" = 0.69, "Petal.Length" = 0.54, "Petal.Width" = 0.48),
+      ideal = c("Sepal.Length" = 0.56, "Sepal.Width" = 0.61, "Petal.Length" = 0.57, "Petal.Width" = 0.70)
+    ),
+    tolerance = 1e-3)
+})
---
+++ tests/testthat/test-item_splithalf.R
@@ -0,0 +1,13 @@
+context("item_split_half")
+
+test_that("item_split_half", {
+  data(iris)
+  x <- iris[, 1:4]
+  expect_equal(
+    item_split_half(x),
+    list(
+      splithalf = 0.7864936,
+      spearmanbrown = 0.8804886
+    ),
+    tolerance = 1e-3)
+})
