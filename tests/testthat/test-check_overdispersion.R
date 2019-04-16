e4f40b48ec0321b57fa0c84d31ae25f91432d64d
Daniel <mail@danielluedecke.de>
2019-04-16 09:34:27 +0200

    Create test-check_overdispersion.R

---
+++ tests/testthat/test-check_overdispersion.R
@@ -0,0 +1,45 @@
+if (require("testthat") && require("performance") && require("glmmTMB")) {
+  context("check_overdispersion")
+
+  data(Salamanders)
+
+  m1 <- glm(count ~ spp + mined, family = poisson, data = Salamanders)
+
+  m2 <- glmmTMB(
+    count ~ mined + spp + (1 | site),
+    family = poisson,
+    data = Salamanders
+  )
+
+  test_that("check_overdispersion", {
+    expect_equal(
+      check_overdispersion(m1),
+      structure(
+        list(
+          chisq_statistic = 1873.71012423995,
+          dispersion_ratio = 2.94608510100621,
+          residual_df = 636L,
+          p_value = 3.26607509162498e-122
+        ),
+        class = "check_overdisp"
+      ),
+      tolerance = 1e-3
+    )
+  })
+
+  test_that("check_overdispersion", {
+    expect_equal(
+      check_overdispersion(m2),
+      structure(
+        list(
+          chisq_statistic = 1475.87512547128,
+          dispersion_ratio = 2.32421279601777,
+          residual_df = 635L,
+          p_value = 8.41489530177729e-69
+        ),
+        class = "check_overdisp"
+      ),
+      tolerance = 1e-3
+    )
+  })
+}
