e099a561c859d46deee61843c41ce83559f5be70
Daniel <mail@danielluedecke.de>
2019-04-16 09:32:08 +0200

    add tests

---
+++ tests/testthat/test-check_convergence.R
@@ -0,0 +1,28 @@
+if (require("testthat") && require("performance") && require("lme4")) {
+  context("check_convergence")
+
+  data(cbpp)
+  data(sleepstudy)
+  set.seed(1)
+  cbpp$x <- rnorm(nrow(cbpp))
+  cbpp$x2 <- runif(nrow(cbpp))
+
+  expect_warning(
+    model <- glmer(
+      cbind(incidence, size - incidence) ~ period + x + x2 + (1 + x | herd),
+      data = cbpp,
+      family = binomial()
+    )
+  )
+
+  test_that("check_convergence", {
+    expect_true(check_convergence(model))
+    expect_equal(check_convergence(model), structure(TRUE, gradient = 0.000280307452338331), tolerance = 1e-3)
+  })
+
+  model <- lmer(Reaction ~ Days + (1 + Days | Subject), data = sleepstudy)
+
+  test_that("check_convergence", {
+    expect_true(check_convergence(model))
+  })
+}
---
+++ tests/testthat/test-check_zeroinflation.R
@@ -0,0 +1,14 @@
+if (require("testthat") && require("performance") && require("glmmTMB")) {
+  context("check_zeroinflation")
+
+  data(Salamanders)
+  m <- glm(count ~ spp + mined, family = poisson, data = Salamanders)
+
+  test_that("check_zeroinflation", {
+    expect_equal(
+      check_zeroinflation(m),
+      structure(list(predicted.zeros = 298, observed.zeros = 387L, ratio = 0.770025839793282, tolerance = 0.05), class = "check_zi"),
+      tolerance = 1e-3
+    )
+  })
+}
