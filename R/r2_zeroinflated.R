555b94e5fa728a52e9dc650459d9a1d3af0aa30c
Daniel <mail@danielluedecke.de>
2019-05-07 08:39:45 +0200

    address #29

--- R/r2_zeroinflated.R
+++ R/r2_zeroinflated.R
@@ -4,11 +4,14 @@
 #' @description Calculates R2 for models with zero-inflation component, including mixed effects models.
 #'
 #' @param model A model.
+#' @param method Indicates the method to calculate R2. See 'Details'. May be abbreviated.
 #'
 #' @return The R2 value.
 #'
-#' @details \code{r2_zeroinflated()} is a correlation-based R2 measure, which
-#'   is rather crude. It simply computes the squared correlation between the
+#' @details The default-method calculates an R2 values based on the residual
+#'   sums of squares (using Pearson residuals), divided by the total sum of
+#'   squares. For \code{method = "correlation"}, R2 is a correlation-based measure,
+#'   which is rather crude. It simply computes the squared correlation between the
 #'   model's actual and predicted reponse.
 #'
 #' @examples
@@ -21,16 +24,34 @@
 #'
 #' r2_zeroinflated(model)
 #'
-#' @importFrom stats cor predict
+#' @importFrom stats cor predict residuals fitted
 #' @importFrom insight model_info get_response
 #' @export
-r2_zeroinflated <- function(model) {
+r2_zeroinflated <- function(model, method = c("default", "correlation")) {
+  method <- match.arg(method)
+
   mi <- insight::model_info(model)
   if (!mi$is_zero_inflated) {
     warning("Model has no zero-inflation component.")
   }
 
+  if (method == "default")
+    .r2_zi_default(model)
+  else
+    .r2_zi_correlation(model)
+}
+
+
+.r2_zi_correlation <- function(model) {
   r2_zi <- stats::cor(insight::get_response(model), stats::predict(model, type = "response"))^2
+  names(r2_zi) <- "R2 for ZI-models"
+  r2_zi
+}
+
+
+.r2_zi_default <- function(model) {
+  r2_zi <- 1 - (sum(stats::fitted(model)^2) /
+                  (sum(stats::fitted(model)^2) + sum(stats::residuals(model, type = "pearson")^2)))
   names(r2_zi) <- "R2 for ZI-models"
   r2_zi
 }
--- tests/testthat/test-r2_zeroinflated.R
+++ tests/testthat/test-r2_zeroinflated.R
@@ -9,8 +9,8 @@ if (require("testthat") && require("performance") && require("pscl")) {
   )
 
   test_that("r2_zeroinflated", {
-    expect_equal(r2_zeroinflated(model), c(`R2 for ZI-models` = 0.0945232333944027), tolerance = 1e-4)
-    expect_equal(r2(model), list(R2 = c(`R2 for ZI-models` = 0.0945232333944027)), tolerance = 1e-4)
+    expect_equal(r2_zeroinflated(model), c(`R2 for ZI-models` = 0.3071805 ), tolerance = 1e-4)
+    expect_equal(r2(model), list(R2 = c(`R2 for ZI-models` = 0.3071805 )), tolerance = 1e-4)
   })
 
   model <- hurdle(
@@ -19,7 +19,7 @@ if (require("testthat") && require("performance") && require("pscl")) {
   )
 
   test_that("r2_zeroinflated", {
-    expect_equal(r2_zeroinflated(model), c(`R2 for ZI-models` = 0.08938686), tolerance = 1e-4)
-    expect_equal(r2(model), list(R2 = c(`R2 for ZI-models` = 0.08938686)), tolerance = 1e-4)
+    expect_equal(r2_zeroinflated(model), c(`R2 for ZI-models` = 0.3178682), tolerance = 1e-4)
+    expect_equal(r2(model), list(R2 = c(`R2 for ZI-models` = 0.3178682)), tolerance = 1e-4)
   })
 }
