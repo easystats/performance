2b420e5a554df68953ad45fe44a48afd625f244b
Daniel <mail@danielluedecke.de>
2019-06-03 23:23:16 +0200

    format docs, print outliers in msg.

--- R/check_outlier.R
+++ R/check_outlier.R
@@ -1,20 +1,20 @@
-#' @title Check for influential observations.
+#' @title Check for influential observations
 #' @name check_outlier
 #'
-#' @description Checks for and locates influential observations (i.e., "outliers") via Cook's Distance
+#' @description Checks for and locates influential observations (i.e., "outliers") via Cook's Distance.
 #'
 #' @param x A model object.
-#' @param print_outliers Optional argument to list individual outliers. Default set as TRUE.
-#' @param drop_outliers Optional argument to drop outliers and return a data frame minus outliers. Default set as FALSE.
+#' @param print_outliers Optional argument to list individual outliers. Default set as \code{TRUE}.
+#' @param drop_outliers Optional argument to drop outliers and return a data frame minus outliers. Default set as \code{FALSE}.
 #' @param ... Currently not used.
 #'
-#' @return Check (message) on whether outliers were detected or not, as well as an optional 
-#' data frame minus outliers (i.e., `drop_outliers = TRUE`)
+#' @return Check (message) on whether outliers were detected or not, as well as an optional
+#' data frame minus outliers (i.e., \code{drop_outliers = TRUE}).
 #'
-#' @details Performs a Cook's distance test to check for influential observations. Those greater than 4/n, are considered 
-#' outliers. This relatively conservative threshold is useful only for detection, rather than justificaiton for automatic observation 
-#' deletion. If users opt to drop observations that may be problematic, they may do so by specifying `drop_outliers = TRUE`.
-#' 
+#' @details Performs a Cook's distance test to check for influential observations. Those greater than 4/n, are considered
+#' outliers. This relatively conservative threshold is useful only for detection, rather than justificaiton for automatic observation
+#' deletion. If users opt to drop observations that may be problematic, they may do so by specifying \code{drop_outliers = TRUE}.
+#'
 #' @references Cook, R. D. (1977). Detection of influential observation in linear regression. Technometrics, 19(1), 15-18.
 #'
 #' @examples
@@ -35,19 +35,21 @@ check_outlier <- function(x, ...) {
 check_outlier.default <- function(x, print_outliers = TRUE, drop_outliers = FALSE, ...) {
   data <- insight::get_data(x)
   n <- nrow(data)
-  
+
   cook <- stats::cooks.distance(x)
-  o <- length(as.numeric(names(cook)[(cook > (4 / n))]))
-  outliers <- ifelse(o >= 1, 1, 0)
-  
+  outliers <- length(as.numeric(names(cook)[(cook > (4 / n))]))
+
   if (outliers >= 1) {
-    insight::print_color(sprintf("Warning: Outliers detected.\n"), 'red')
-  } else insight::print_color(sprintf("OK: No outliers detected.\n"), 'green')
-  
-  if (print_outliers == TRUE) {
-    print((names(cook)[(cook > (4 / n))]))
+    if (print_outliers) {
+      o <- paste0(" (cases ", paste0(names(cook)[(cook > (4 / n))], collapse = ", "), ")")
+    } else {
+      o <- ""
+    }
+    insight::print_color(sprintf("Warning: Outliers detected%s.\n", o), 'red')
+  } else {
+    insight::print_color("OK: No outliers detected.\n", 'green')
   }
-  
+
   if (drop_outliers == TRUE) {
     data_dropped <- data[!(cook > (4 / n)), ]
     return(data_dropped)
