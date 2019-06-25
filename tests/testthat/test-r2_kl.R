0521dd0684f30c053ae12d05b8ee81bb75988d70
Daniel Lüdecke <mail@danielluedecke.de>
2019-06-25 08:48:58 +0200

    fix test issues

--- tests/testthat/test-r2_kl.R
+++
@@ -1,9 +0,0 @@
-if (require("testthat") && require("performance")) {
-  context("r2_kl")
-
-  test_that("r2_kl", {
-    model <- glm(vs ~ wt + mpg, data = mtcars, family = "binomial")
-    expect_equal(r2_kl(model), c(`Kullback-Leibler R2` = 0.383436177754933), tolerance = 1e-3)
-    expect_equal(r2_kl(model, adjust = FALSE), c(`Kullback-Leibler R2` = 0.423214488867518), tolerance = 1e-3)
-  })
-}
---
+++ tests/testthat/test-r2_kullback.R
@@ -0,0 +1,9 @@
+if (require("testthat") && require("performance")) {
+  context("r2_kullback")
+
+  test_that("r2_kullback", {
+    model <- glm(vs ~ wt + mpg, data = mtcars, family = "binomial")
+    expect_equal(r2_kullback(model), c(`Kullback-Leibler R2` = 0.383436177754933), tolerance = 1e-3)
+    expect_equal(r2_kullback(model, adjust = FALSE), c(`Kullback-Leibler R2` = 0.423214488867518), tolerance = 1e-3)
+  })
+}
