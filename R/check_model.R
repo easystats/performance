a26c9a2b8102afab011ae344bca133a24830b045
Daniel Lüdecke <mail@danielluedecke.de>
2019-09-05 17:42:25 +0200

    fix issue

--- R/check_model.R
+++ R/check_model.R
@@ -80,7 +80,6 @@ check_model.default <- function(x, dot_size = 2, line_size = .8, panel = TRUE, c
   dat$NCV <- .diag_ncv(model)
   dat$HOMOGENEITY <- .diag_homogeneity(model)
   dat$OUTLIERS <- check_outliers(model)
-  if (!is.null(dat$OUTLIERS)) class(dat$OUTLIERS) <- "data.frame"
 
   dat <- .compact_list(dat)
   class(dat) <- c("check_model", "see_check_model")
@@ -98,7 +97,6 @@ check_model.default <- function(x, dot_size = 2, line_size = .8, panel = TRUE, c
   dat$HOMOGENEITY <- .diag_homogeneity(model)
   dat$REQQ <- .diag_reqq(model, level = .95, model_info = model_info)
   dat$OUTLIERS <- check_outliers(model)
-  if (!is.null(dat$OUTLIERS)) class(dat$OUTLIERS) <- "data.frame"
 
   dat <- .compact_list(dat)
   class(dat) <- c("check_model", "see_check_model")
