04f8b806fc17969ac2c40e579152d3e6a7b8a1ea
Daniel Lüdecke <mail@danielluedecke.de>
2019-10-25 13:49:19 +0200

    add test

--- tests/testthat/test-backticks.R
+++ tests/testthat/test-backticks.R
@@ -1,5 +1,5 @@
 if (require("testthat") && require("performance")) {
-  context("check_convergence")
+  context("backticks")
 
   data(iris)
   iris$`a m` <- iris$Species
@@ -7,4 +7,8 @@ if (require("testthat") && require("performance")) {
   m1 <- lm(`Sepal Width` ~ Petal.Length + `a m` * log(Sepal.Length), data = iris)
   m2 <- lm(Sepal.Width ~ Petal.Length + Species * log(Sepal.Length), data = iris)
 
+  test_that("check_collinearity, backticks", {
+    expect_equal(check_collinearity(m1)$Parameter, c("Petal.Length", "a m", "log(Sepal.Length)", "a m:log(Sepal.Length)"))
+    expect_equal(check_collinearity(m2)$Parameter, c("Petal.Length", "Species", "log(Sepal.Length)", "Species:log(Sepal.Length)"))
+  })
 }
