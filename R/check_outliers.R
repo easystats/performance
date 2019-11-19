6e2364af082341ccf902f1fb9432d0561929f4c4
Dominique Makowski <dom.mak19@gmail.com>
2019-11-19 15:02:47 +0800

    Update check_outliers.R

--- R/check_outliers.R
+++ R/check_outliers.R
@@ -657,7 +657,7 @@ as.numeric.check_outliers <- function(x, ...){
   }
 
   # Compute
-  if (utils::packageVersion("solitude") < '0.2.1') {
+  if (utils::packageVersion("solitude") < '0.2.0') {
     iforest <- solitude::isolationForest(x)
     out$Distance_iforest <- predict(iforest, x, type = "anomaly_score")
   } else{
