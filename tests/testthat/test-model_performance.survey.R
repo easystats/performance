4c81f7492eabd6e1241d20f9d8c84180c7c1dc38
Daniel <mail@danielluedecke.de>
2020-03-23 12:57:06 +0100

    skip test on Travis

--- tests/testthat/test-model_performance.survey.R
+++ tests/testthat/test-model_performance.survey.R
@@ -1,50 +1,54 @@
-if (require("testthat") && require("performance") && require("survey")) {
-  test_that("model_performance.survey", {
-    set.seed(123)
-    data(api)
-    dstrat <- survey::svydesign(
-      id = ~1,
-      strata = ~stype,
-      weights = ~pw,
-      data = apistrat,
-      fpc = ~fpc
-    )
+.runThisTest <- Sys.getenv("RunAllperformanceTests") == "yes"
 
-    # model
-    model <- survey::svyglm(
-      formula = sch.wide ~ ell + meals + mobility,
-      design = dstrat,
-      family = quasibinomial()
-    )
+if (.runThisTest && Sys.getenv("USER") != "travis") {
+  if (require("testthat") && require("performance") && require("survey")) {
+    test_that("model_performance.survey", {
+      set.seed(123)
+      data(api)
+      dstrat <- survey::svydesign(
+        id = ~1,
+        strata = ~stype,
+        weights = ~pw,
+        data = apistrat,
+        fpc = ~fpc
+      )
 
-    mp <- suppressWarnings(model_performance(model))
+      # model
+      model <- survey::svyglm(
+        formula = sch.wide ~ ell + meals + mobility,
+        design = dstrat,
+        family = quasibinomial()
+      )
 
-    expect_equal(mp$R2, 0.02943044, tolerance = 0.01)
-    expect_equal(mp$AIC, 183.9723, tolerance = 0.01)
-    expect_equal(colnames(mp), c("AIC", "R2", "R2_adjusted", "RMSE", "SCORE_LOG", "PCP"))
-  })
+      mp <- suppressWarnings(model_performance(model))
+
+      expect_equal(mp$R2, 0.02943044, tolerance = 0.01)
+      expect_equal(mp$AIC, 183.9723, tolerance = 0.01)
+      expect_equal(colnames(mp), c("AIC", "R2", "R2_adjusted", "RMSE", "SCORE_LOG", "PCP"))
+    })
 
 
-  test_that("model_performance.survey-cox", {
-    data(pbc)
-    pbc$randomized <- with(pbc, !is.na(trt) & trt > 0)
-    biasmodel <- glm(randomized ~ age * edema, data = pbc, family = binomial)
-    pbc$randprob <- fitted(biasmodel)
-    if (is.null(pbc$albumin)) pbc$albumin <- pbc$alb ##pre2.9.0
+    test_that("model_performance.survey-cox", {
+      data(pbc)
+      pbc$randomized <- with(pbc, !is.na(trt) & trt > 0)
+      biasmodel <- glm(randomized ~ age * edema, data = pbc, family = binomial)
+      pbc$randprob <- fitted(biasmodel)
+      if (is.null(pbc$albumin)) pbc$albumin <- pbc$alb ##pre2.9.0
 
-    dpbc <- svydesign(
-      id =  ~ 1,
-      prob =  ~ randprob,
-      strata =  ~ edema,
-      data = subset(pbc, randomized)
-    )
-    rpbc <- as.svrepdesign(dpbc)
-    model <- svycoxph(Surv(time, status > 0) ~ log(bili) + protime + albumin, design = dpbc)
+      dpbc <- svydesign(
+        id =  ~ 1,
+        prob =  ~ randprob,
+        strata =  ~ edema,
+        data = subset(pbc, randomized)
+      )
+      rpbc <- as.svrepdesign(dpbc)
+      model <- svycoxph(Surv(time, status > 0) ~ log(bili) + protime + albumin, design = dpbc)
 
-    mp <- suppressWarnings(model_performance(model))
+      mp <- suppressWarnings(model_performance(model))
 
-    expect_equal(mp$R2, 0.02943044, tolerance = 0.01)
-    expect_equal(mp$AIC, 183.9723, tolerance = 0.01)
-    expect_equal(colnames(mp), c("AIC", "R2", "R2_adjusted", "RMSE", "SCORE_LOG", "PCP"))
-  })
+      expect_equal(mp$R2, 0.02943044, tolerance = 0.01)
+      expect_equal(mp$AIC, 183.9723, tolerance = 0.01)
+      expect_equal(colnames(mp), c("AIC", "R2", "R2_adjusted", "RMSE", "SCORE_LOG", "PCP"))
+    })
+  }
 }
