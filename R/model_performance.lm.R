0a5259f2adcceb5a1d7a413055d9c3053a17d1e3
Daniel <mail@danielluedecke.de>
2020-04-07 11:01:43 +0200

    minor fix for mlogit

--- R/model_performance.lm.R
+++ R/model_performance.lm.R
@@ -124,9 +124,6 @@ model_performance.iv_robust <- model_performance.lm
 model_performance.ivreg <- model_performance.lm
 
 #' @export
-model_performance.mlogit <- model_performance.lm
-
-#' @export
 model_performance.multinom <- model_performance.lm
 
 #' @export
@@ -158,3 +155,15 @@ model_performance.flexsurvreg <- model_performance.lm
 
 #' @export
 model_performance.hurdle <- model_performance.lm
+
+
+
+#' @export
+model_performance.mlogit <- function(model, metrics = "all", verbose = TRUE, ...) {
+  if (requireNamespace("mlogit", quietly = TRUE)) {
+    model_performance.lm(model = model, metrics = metrics, verbose = verbose, ...)
+  } else {
+    NULL
+  }
+}
+
