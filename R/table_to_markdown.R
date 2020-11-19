06251f48d0a6a8ce922af3cfde25d2d351d99f41
Daniel <mail@danielluedecke.de>
2020-11-19 22:39:54 +0100

    add to_table

---
+++ R/table_to_markdown.R
@@ -0,0 +1,15 @@
+#' @rdname to_table.performance_model
+#' @export
+table_to_markdown.performance_model <- function(x, digits = 2, ...) {
+  to_table(x = x, digits = digits, format = "markdown", ...)
+}
+
+#' @export
+table_to_markdown.compare_performance <- table_to_markdown.performance_model
+
+
+# Reexports models ------------------------
+
+#' @importFrom insight table_to_markdown
+#' @export
+insight::table_to_markdown
