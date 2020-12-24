9c50f0d6c0fcf3b7f18c79a5c381da86de96b7f9
Daniel <mail@danielluedecke.de>
2020-12-24 16:22:27 +0100

    Update test-performance_lrt.R

--- tests/testthat/test-performance_lrt.R
+++ tests/testthat/test-performance_lrt.R
@@ -11,17 +11,21 @@ if (require("testthat") &&
     rez <- performance_lrt(m1, m2, m3, estimator = "OLS")
 
     ref <- anova(m1, m2, m3, test = "LRT")
-    testthat::expect_equal(ref$`Pr(>Chi)`, rez$p, tolerance = 1e-03)
+    expect_equal(ref$`Pr(>Chi)`, rez$p, tolerance = 1e-03)
 
     # lrtest
     rez <- performance_lrt(m1, m2, m3, estimator = "ML")
 
     # ref <- lmtest::lrtest(m1, m2, m3)
     # ref$`Pr(>Chisq)`
-    testthat::expect_equal(c(NA, 0.4747344, 0.5302030), rez$p, tolerance = 1e-03)
+    expect_equal(c(NA, 0.4747344, 0.5302030), rez$p, tolerance = 1e-03)
     # ref$LogLik
-    testthat::expect_equal(c(-74.00503, -73.74957, -73.55256), rez$LogLik, tolerance = 1e-03)
+    expect_equal(c(-74.00503, -73.74957, -73.55256), rez$LogLik, tolerance = 1e-03)
     # ref$Chisq
-    testthat::expect_equal(c(NA, 0.5109349, 0.3940024), rez$Chi2, tolerance = 1e-03)
+    expect_equal(c(NA, 0.5109349, 0.3940024), rez$Chi2, tolerance = 1e-03)
+
+    rez <- performance_lrt(m3, m1, m2, estimator = "OLS")
+    expect_equal(rez$Chi2, c(NA, 2.27227, 0.4703), tolerance = 1e-03)
+
   })
 }
