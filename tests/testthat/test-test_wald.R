d0921a5f3ed58d06c2648211166c422901a312f6
DominiqueMakowski <dom.mak19@gmail.com>
2021-01-11 23:15:05 +0800

    Update test-test_wald.R

--- tests/testthat/test-test_wald.R
+++ tests/testthat/test-test_wald.R
@@ -1,7 +1,8 @@
 if (require("testthat") &&
-  require("lavaan")) {
+  require("lavaan") &&
+  require("lmtest")) {
 
-  test_that("test_wald", {
+  test_that("test_wald - lm", {
     m1 <- lm(Sepal.Length ~ Petal.Width * Species, data = iris)
     m2 <- lm(Sepal.Length ~ Petal.Width + Species, data = iris)
     m3 <- lm(Sepal.Length ~ Petal.Width, data = iris)
@@ -11,5 +12,18 @@ if (require("testthat") &&
 
     testthat::expect_equal(rez$`F`, ref$`F`)
     testthat::expect_equal(rez$p, ref$`Pr(>F)`)
+  })
+
+  test_that("test_wald - glm", {
+    m1 <- glm(vs ~ disp + hp + drat, data = mtcars, family="binomial")
+    m2 <- glm(vs ~ disp + hp, data = mtcars, family="binomial")
+    m3 <- glm(vs ~ disp, data = mtcars, family="binomial")
+
+    rez <- test_wald(m1, m2, m3)
+    # Why different?
+    # ref <- lmtest::waldtest(m1, m2, m3)
+    #
+    # testthat::expect_equal(rez$`F`, ref$`F`)
+    # testthat::expect_equal(rez$p, ref$`Pr(>F)`)
   })
 }
