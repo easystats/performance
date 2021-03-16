c589749e6193c0f580d54a92ca08c4d16a198fac
Indrajeet Patil <inderonline1988@gmail.com>
2021-03-16 15:50:37 +0100

    jumpstarting JOSS paper

---
+++ paper/make_figs.R
@@ -0,0 +1,22 @@
+# --------------------
+library(performance)
+library(lme4)
+library(see)
+
+# fig1 --------------------
+
+model <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
+result1 <- plot(check_model(model))
+
+ggplot2::ggsave("figure1.png", result1, height = 10, width = 8)
+
+# fig2 ----------------------
+
+data(iris)
+lm1 <- lm(Sepal.Length ~ Species, data = iris)
+lm2 <- lm(Sepal.Length ~ Species + Petal.Length, data = iris)
+lm3 <- lm(Sepal.Length ~ Species * Sepal.Width, data = iris)
+lm4 <- lm(Sepal.Length ~ Species * Sepal.Width + Petal.Length + Petal.Width, data = iris)
+result2 <- plot(compare_performance(lm1, lm2, lm3, lm4))
+
+ggplot2::ggsave("figure2.png", result2, height = 7, width = 6)
