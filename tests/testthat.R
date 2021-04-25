4df4b49ae6b86ad6ed7eac6e82cd275097880fed
Daniel <mail@danielluedecke.de>
2021-04-25 13:06:27 +0200

    test conditional

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,26 +1,27 @@
-library(testthat)
-library(performance)
+if (require("testthat")) {
+  library(performance)
 
-if (length(strsplit(packageDescription("performance")$Version, "\\.")[[1]]) > 3) {
-  Sys.setenv("RunAllperformanceTests" = "yes")
-} else {
-  Sys.setenv("RunAllperformanceTests" = "no")
-}
+  if (length(strsplit(packageDescription("performance")$Version, "\\.")[[1]]) > 3) {
+    Sys.setenv("RunAllperformanceTests" = "yes")
+  } else {
+    Sys.setenv("RunAllperformanceTests" = "no")
+  }
 
-osx <- tryCatch(
-  {
-    si <- Sys.info()
-    if (!is.null(si["sysname"])) {
-      si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
-    } else {
+  osx <- tryCatch(
+    {
+      si <- Sys.info()
+      if (!is.null(si["sysname"])) {
+        si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+      } else {
+        FALSE
+      }
+    },
+    error = function(e) {
       FALSE
     }
-  },
-  error = function(e) {
-    FALSE
-  }
-)
+  )
 
-if (!osx) {
-  test_check("performance")
+  if (!osx) {
+    test_check("performance")
+  }
 }
