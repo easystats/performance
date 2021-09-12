4dd9a2c2d55b916a47e793990f1af28125669141
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2021-09-12 10:25:29 -0400

    Update tests

--- tests/testthat/test-model_performance.bayesian.R
+++ tests/testthat/test-model_performance.bayesian.R
@@ -11,21 +11,21 @@ if (.runThisTest &&
     perf <- model_performance(model)
 
     expect_equal(perf$R2, 0.7398733, tolerance = 1e-3)
-    expect_equal(perf$R2_adjusted, 0.7075685, tolerance = 1e-3)
+    expect_equal(perf$R2_adjusted, 0.7162912, tolerance = 1e-3)
     expect_equal(perf$ELPD, -83.49838, tolerance = 1e-3)
 
     model <- insight::download_model("stanreg_lm_2")
     perf <- model_performance(model)
 
     expect_equal(perf$R2, 0.8168386, tolerance = 1e-3)
-    expect_equal(perf$R2_adjusted, 0.791236, tolerance = 1e-3)
+    expect_equal(perf$R2_adjusted, 0.7979026, tolerance = 1e-3)
     expect_equal(perf$ELPD, -78.38735, tolerance = 1e-3)
 
     model <- insight::download_model("stanreg_lmerMod_1")
     perf <- model_performance(model)
 
     expect_equal(perf$R2, 0.6286546, tolerance = 1e-3)
-    expect_equal(perf$R2_adjusted, 0.5896637, tolerance = 1e-3)
+    expect_equal(perf$R2_adjusted, 0.6053507, tolerance = 1e-3)
     expect_equal(perf$ELPD, -31.55849, tolerance = 1e-3)
   })
 
@@ -36,13 +36,13 @@ if (.runThisTest &&
     model <- insight::download_model("brms_1")
     perf <- model_performance(model)
     expect_equal(perf$R2, 0.8262673, tolerance = 1e-3)
-    expect_equal(perf$R2_adjusted, 0.7901985, tolerance = 1e-3)
+    expect_equal(perf$R2_adjusted, 0.7982615, tolerance = 1e-3)
     expect_equal(perf$ELPD, -78.59823, tolerance = 1e-3)
 
     model <- insight::download_model("brms_mixed_4")
     perf <- model_performance(model)
     expect_equal(perf$R2, 0.954538, tolerance = 1e-3)
-    expect_equal(perf$R2_adjusted, 0.9523081, tolerance = 1e-3)
+    expect_equal(perf$R2_adjusted, 0.9529004, tolerance = 1e-3)
     expect_equal(perf$ELPD, -70.40493, tolerance = 1e-3)
 
     model <- insight::download_model("brms_ordinal_1")
