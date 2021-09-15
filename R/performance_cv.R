4997c645bf06e4357349acb44d9313dd91a41ffb
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2021-09-15 17:20:13 -0400

    Add non-stacked CV metrics

--- R/performance_cv.R
+++ R/performance_cv.R
@@ -10,18 +10,25 @@
 #'   that will be used as the cross-validation sample.
 #' @param method Character string, indicating the cross-validation method to use:
 #'   whether holdout (`"holdout"`, aka train-test), k-fold (`"k_fold"`), or
-#'   leave-one-out (`"loo"`). If `data` is suppled
+#'   leave-one-out (`"loo"`). If `data` is supplied, this argument is ignored.
 #' @param metrics Can be "all", "common" or a character vector of metrics to be
 #'   computed (some of c("Deviance", "MSE", "RMSE", "R2")). "common" will
 #'   compute R2 and RMSE.
 #' @param prop If `method = "holdout"`, what proportion of the sample to hold
 #'   out as the test sample?
 #' @param k If `method = "k_fold"`, the number of folds to use.
+#' @param stack Logical. If `method = "k_fold"`, should performance be computed
+#'   by stacking residuals from each holdout fold and calculating each metric on
+#'   the stacked data (`TRUE`, default) or should performance be computed by
+#'   calculating metrics within each holdout fold and averaging performance
+#'   across each fold (`FALSE`)?
 #' @param verbose Toggle warnings.
 #' @param ... Not used.
 #'
 #' @return A data frame with columns for each metric requested, as well as `k`
-#'   if `method = "holdout"` and the `Method` used for cross-validation.
+#'   if `method = "holdout"` and the `Method` used for cross-validation. If
+#'   `method = "holdout"` and `stack = TRUE`, the standard error (standard
+#'   deviation across holdout folds) for each metric is also included.
 #'
 #' @examples
 #' model <- lm(mpg ~ wt + cyl, data = mtcars)
@@ -30,13 +37,20 @@
 #' @export
 performance_cv <- function(
   model,
+  data = NULL,
   method = c("holdout", "k_fold", "loo"),
   metrics = "all",
   prop = .30,
   k = 5,
+  stack = TRUE,
   verbose = TRUE,
   ...
 ) {
+  if (metrics == "all") {
+    metrics <- c("MSE", "RMSE", "R2")
+  } else if (metrics == "common") {
+    metrics <- c("RMSE", "R2")
+  }
   method <- match.arg(method, choices = c("holdout", "k_fold"))
   formula <- stats::formula(model)
   resp.name <- insight::find_response(model)
@@ -44,6 +58,7 @@ performance_cv <- function(
   info <- insight::model_info(model, verbose = verbose)
   if (info$is_linear) {
     if (!is.null(data)) {
+      stack <- TRUE
       test_resp <- data[, resp.name]
       test_pred <- insight::get_predicted(model, data = data)
       test_resd <- test_resp - test_pred
@@ -54,30 +69,54 @@ performance_cv <- function(
       test_pred <- insight::get_predicted(model_upd, data = model_data[-train_i, ])
       test_resd <- test_resp - test_pred
     } else {
+      if (method == "loo") {
+        stack <- TRUE
+        k = length(test_pred)
+      }
       cv_folds <- .crossv_kfold(model_data, k = k)
       models_upd <- lapply(cv_folds, function(.x) {
         stats::update(model, data = model_data[.x$train, ])
       })
-      test_pred <- unlist(mapply(function(.x, .y) {
+      test_pred <- mapply(function(.x, .y) {
         insight::get_predicted(.y, data = model_data[.x$test, ])
-      }, cv_folds, models_upd, SIMPLIFY = FALSE))
-      test_resp <- unlist(lapply(cv_folds, function(.x) {
+      }, cv_folds, models_upd, SIMPLIFY = FALSE)
+      test_resp <- lapply(cv_folds, function(.x) {
         as.data.frame(model_data[.x$test, ])[[resp.name]]
-      }))
-      test_resd <- unlist(test_resp) - unlist(test_pred)
+      })
     }
   } else {
     warning("Only linear models currently supported.", call. = FALSE)
     return(NULL)
   }
-  MSE <- mean(test_resd^2, na.rm = TRUE)
-  RMSE <- sqrt(MSE)
-  R2 <- 1 - MSE / mean((test_resp - mean(test_resp, na.rm = TRUE))^2)
-  out <- data.frame(MSE = MSE, RMSE = RMSE, R2 = R2, method = method)
+  if (isTRUE(stack)) {
+    test_resp <- unlist(test_resp)
+    test_pred <- unlist(test_pred)
+    test_resd <- test_resp - test_pred
+    MSE <- mean(test_resd^2, na.rm = TRUE)
+    RMSE <- sqrt(MSE)
+    R2 <- 1 - MSE / mean((test_resp - mean(test_resp, na.rm = TRUE))^2, na.rm = TRUE)
+    out <- data.frame(MSE = MSE, RMSE = RMSE, R2 = R2)
+  } else {
+    test_resd <- mapply(function(.x, .y) {
+      .x - .y
+    }, test_resp, test_pred, SIMPLIFY = FALSE)
+    MSEs <- sapply(test_resd, function(x) mean(x^2, na.rm = TRUE))
+    RMSEs <- sqrt(MSEs)
+    resp_vars <- sapply(test_resp, function(x) mean((x - mean(x, na.rm = TRUE))^2, na.rm = TRUE))
+    R2s <- 1 - MSEs / resp_vars
+    out <- data.frame(
+      MSE = mean(MSEs), MSE_SE = sd(MSEs),
+      RMSE = mean(RMSEs), RMSE_SE = sd(RMSEs),
+      R2 = mean(R2s), R2_SE = sd(R2s)
+    )
+  }
+
+  out <- out[, colnames(out) %in% c(metrics, paste0(metrics, "_SE"))]
+  out$Method <- method
   out$k <- if (method == "k_fold") k else NULL
-  out <- out[, colnames(out) %in% c("k", metrics)]
   if ("deviance" %in% metrics) mesage("Metric 'deviance' not yet implemented.")
   class(out) <- c("performance_cv", "data.frame")
+  return(out)
 }
 
 # TODO: implement performance::log_lik() function
