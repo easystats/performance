dad9f1094fa29671eb061aede129b5290c88c262
Daniel <mail@danielluedecke.de>
2022-02-27 09:47:29 +0100

    #154

--- R/plot-methods.R
+++ R/plot-methods.R
@@ -70,3 +70,15 @@ plot.check_model <- function(x, ...) {
   insight::check_if_installed("see", "for model diagnositic plots")
   NextMethod()
 }
+
+#' @export
+plot.test_likelihoodratio <- function(x, ...) {
+  warning(insight::format_message("There is currently no plot() method for test-functions.",
+                                  "Please use 'plot(compare_perfomance())' for some visual representations of your model comparisons."), call. = FALSE)
+}
+
+#' @export
+plot.test_performance <- function(x, ...) {
+  warning(insight::format_message("There is currently no plot() method for test-functions.",
+                                  "Please use 'plot(compare_perfomance())' for some visual representations of your model comparisons."), call. = FALSE)
+}
