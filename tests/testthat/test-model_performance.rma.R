afcde591f02177a7b9a8e38819892ace0f16f725
Daniel <mail@danielluedecke.de>
2022-03-01 20:37:05 +0100

    update model_performance.rma, update tests

--- tests/testthat/test-model_performance.rma.R
+++ tests/testthat/test-model_performance.rma.R
@@ -3,7 +3,7 @@ if (requiet("testthat") && requiet("performance") && requiet("metafor")) {
 
   test_that("model_performance.rma", {
     model <- rma(yi, vi, data = dat, method = "REML")
-    mp <- model_performance(model)
+    mp <- model_performance(model, estimator = "REML")
     expect_null(mp$R2)
     expect_equal(mp$AIC, 28.40474, tolerance = 1e-3)
     expect_equal(mp$I2, 0.9222139, tolerance = 1e-3)
@@ -15,7 +15,7 @@ if (requiet("testthat") && requiet("performance") && requiet("metafor")) {
 
   test_that("model_performance.rma", {
     model <- rma(yi, vi, mods = cbind(ablat, year), data = dat)
-    mp <- model_performance(model)
+    mp <- model_performance(model, estimator = "REML")
     expect_equal(mp$R2, 0.6463217, tolerance = 1e-3)
     expect_equal(mp$AIC, 24.21375, tolerance = 1e-3)
     expect_equal(mp$I2, 0.719778, tolerance = 1e-3)
