e3f95b6bcce1b931d7774d386732a029f426f711
Dominique Makowski <dom.mak19@gmail.com>
2022-03-18 13:31:33 +0800

    initialize function

---
+++ tests/testthat/test-check_smooth.R
@@ -0,0 +1,35 @@
+if (requiet("testthat") && requiet("performance") && requiet("mgcv") && requiet("brms")) {
+  test_that("check_smooth | mgcv", {
+    skip_if_not_installed("mgcv")
+    set.seed(333)
+
+    m1 <- mgcv::gam(Sepal.Length ~ s(Petal.Length, k = 3) + s(Petal.Width, k=5), data = iris)
+    m2 <- mgcv::gam(Sepal.Length ~ t2(Petal.Length, Petal.Width, k=5), data = iris)
+    m3 <- mgcv::gam(Sepal.Length ~ s(Petal.Length, k = 3, by = Species), data = iris)
+
+    rez1 <- check_smooth(m1, iterations=1000)
+    rez2 <- check_smooth(m2, iterations=1000)
+    rez3 <- check_smooth(m3, iterations=1000)
+
+    c1 <- as.data.frame(mgcv::k.check(m1, n.rep=1000))
+    c2 <- as.data.frame(mgcv::k.check(m2, n.rep=1000))
+    c3 <- as.data.frame(mgcv::k.check(m3, n.rep=1000))
+
+    # Deterministic
+    expect_equal(max(rez1$EDF_max - c1$`k'`), 0, tolerance = 0)
+    expect_equal(max(rez1$EDF - c1$edf), 0, tolerance = 0)
+
+    # Random
+    expect_equal(max(rez1$k - c1$`k-index`), 0, tolerance = 0.1)
+    expect_equal(max(rez1$p - c1$`p-value`), 0, tolerance = 0.1)
+  })
+
+  test_that("check_smooth | brms", {
+    skip_if_not_installed("brms")
+
+    # m1 <- brms::brm(Sepal.Length ~ s(Petal.Length, k = 3) + s(Petal.Width, k=5), data = iris,
+    #                 refresh = 0, iter = 200, algorithm = "meanfield")
+
+    # brms models currently not supported
+  })
+}
