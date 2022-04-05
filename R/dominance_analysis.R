29c359be40b715fe0310ed9573dacf5005722bf6
Daniel <mail@danielluedecke.de>
2022-04-05 11:09:09 +0200

    format messages

--- R/dominance_analysis.R
+++ R/dominance_analysis.R
@@ -3,8 +3,7 @@
 #'
 #' @description Computes Dominance Analysis Statistics and Designations
 #'
-#' @param model An `{insight}`- and `performance::r2`-supported model object
-#' of specific classes.  See Notes section.
+#' @param model A model object supported by `performance::r2()`. See Notes section.
 #' @param ...  Not used at current.
 #'
 #' @return Object of class "dominance_analysis".
@@ -36,7 +35,7 @@
 #'
 #' @section Notes:
 #'
-#' The input model is parsed using `insight::find_predictors`, does not
+#' The input model is parsed using `insight::find_predictors()`, does not
 #' yet support interactions, transformations, or offsets applied in the
 #' R formula, and will fail with an error if any such terms are detected.
 #'
@@ -45,17 +44,17 @@
 #' the model is estimated as a `data` argument.  Formulas submitted
 #' using object references (i.e., `lm(mtcars$mpg ~ mtcars$vs)`) and
 #' functions that accept data as a non-`data` argument
-#' (e.g., `survey::svyglm` uses `design`) will fail.
+#' (e.g., `survey::svyglm()` uses `design`) will fail.
 #'
-#' Models that return \code{TRUE} for the \code{insight::model_info}
+#' Models that return `TRUE` for the `insight::model_info()`
 #' function's values "is_bayesian", "is_mixed", "is_gam",
 #' is_multivariate", "is_zero_inflated",
 #' or "is_hurdle" are not supported at current.
 #'
-#' When `performance::r2` returns multiple values, only the first is used
+#' When `performance::r2()` returns multiple values, only the first is used
 #' by default.
 #'
-#' The underlying `domir::domin` function that implements the dominance
+#' The underlying `domir::domin()` function that implements the dominance
 #' statistic and designation computations has only been tested to R version
 #' 3.5 and will fail with an error if called in R versions < 3.5.
 #'
@@ -88,38 +87,60 @@
 #' @author Joseph Luchman
 #'
 #' @examples
-#' if (require("domir") && (R.version$major > 3 | (R.version$major == 3 && R.version$minor >= 5))) {
+#' if (require("domir")) {
 #'   model <- glm(vs ~ cyl + carb + mpg, data = datasets::mtcars,
 #'            family = binomial())
 #'
 #'   r2(model)
-#'
 #'   dominance_analysis(model)
-#'
 #'}
 #'
 #' @export
-
 dominance_analysis <- function(model, ...) {
 
   # Exit Conditions
-  if (!requireNamespace("domir", quietly = TRUE)) stop("package domir is required.")
+  insight::check_if_installed("domir")
 
-  if (!insight::is_regression_model(model)) stop(paste(deparse(substitute(model)), "is not a supported insight model.\nYou may be able to dominance analyze this model using the domir package."))
+  if (!insight::is_regression_model(model)) {
+    stop(insight::format_message(
+      paste(deparse(substitute(model)), "is not a supported insight model."),
+      "You may be able to dominance analyze this model using the domir package."
+    ), call. = FALSE)
+  }
 
-  if (!any(utils::methods(class = class(model)[[1]])==paste0("r2.", class(model)[[1]]))) stop(paste(deparse(substitute(model)), "does not have a pefromance-supported r2 method.\nYou may be able to dominance analyze this model using the domir package."))
+  if (!any(utils::methods(class = class(model)[[1]]) == paste0("r2.", class(model)[[1]]))) {
+    stop(insight::format_message(
+      paste(deparse(substitute(model)), "does not have a pefromance-supported r2 method."),
+      "You may be able to dominance analyze this model using the domir package."
+    ), call. = FALSE)
+  }
 
   model_info <- insight::model_info(model)
-  if (any(unlist(model_info[c("is_bayesian", "is_mixed", "is_gam", "is_multivariate", "is_zero_inflated", "is_hurdle")])))
-    stop(paste0("dominance_analysis does not yet support models of class ", class(model), ".\nYou may be able to dominance analyze this model using the domir package."))
+  if (any(unlist(model_info[c("is_bayesian", "is_mixed", "is_gam", "is_multivariate", "is_zero_inflated", "is_hurdle")]))) {
+    stop(insight::format_message(
+      paste0("`dominance_analysis()` does not yet support models of class ", class(model), "."),
+      "You may be able to dominance analyze this model using the domir package."
+    ), call. = FALSE)
+  }
 
-  if (!is.null(insight::find_interactions(model))) stop("interactions in the model formula are not allowed.")
+  if (!is.null(insight::find_interactions(model))) {
+    stop("Interactions in the model formula are not allowed.", call. = FALSE)
+  }
 
-  if (!all(insight::find_predictors(model)$conditional %in% attr(stats::terms(insight::find_formula(model)$conditional), "term.labels"))) stop("predictors do not match terms.\nThis usually occurs when there are in-formula predictor transformations such as log(x) or I(x+z).")
+  if (!all(insight::find_predictors(model)$conditional %in% attr(stats::terms(insight::find_formula(model)$conditional), "term.labels"))) {
+    stop(insight::format_message(
+      "Predictors do not match terms.",
+      "This usually occurs when there are in-formula predictor transformations such as log(x) or I(x+z)."
+    ), call. = FALSE)
+  }
 
-  if (!is.null(insight::find_offset(model))) stop("offsets in the model formula are not allowed.")
+  if (!is.null(insight::find_offset(model))) {
+    stop("Offsets in the model formula are not allowed.", call. = FALSE)
+  }
 
-  if (!(R.version$major > 3 | (R.version$major == 3 && R.version$minor >= 5))) stop("R versions < 3.5 not supported")
+  if (getRversion() < "3.5") {
+    stop("R versions < 3.5 not supported.", call. = FALSE)
+  }
 
   # Collect components for arguments
   ivs <- insight::find_predictors(model, flatten = TRUE)
@@ -144,14 +165,12 @@ dominance_analysis <- function(model, ...) {
 
   da_res <- da_res[which(names(da_res) %in% c("Fit_Statistic_Overall", "General_Dominance", "Conditional_Dominance", "Complete_Dominance", "Standardized", "Ranks"))]
 
-  names(da_res)<- c("general_dominance", "standardized", "ranks", "conditional_dominance", "complete_dominance", "model_R2")
+  names(da_res) <- c("general_dominance", "standardized", "ranks", "conditional_dominance", "complete_dominance", "model_R2")
 
   dimnames(da_res$complete_dominance) <- list(colnames(da_res$complete_dominance), names(da_res$general_dominance))
 
   da_res$complete_dominance <- t(da_res$complete_dominance)
 
   class(da_res) <- c("dominance_analysis", "domin", "list")
-
-  return(da_res)
-
+  da_res
 }
