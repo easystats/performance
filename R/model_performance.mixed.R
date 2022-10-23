b362650d8d3f8c2cc29eb90ef94b9360be317fdb
Daniel <mail@danielluedecke.de>
2022-10-23 14:16:20 +0200

    code style

--- R/model_performance.mixed.R
+++ R/model_performance.mixed.R
@@ -76,14 +76,8 @@ model_performance.merMod <- function(model,
   }
 
   if ("AICC" %in% toupper(metrics)) {
-    out$AICc <- tryCatch(
-      {
-        performance_aicc(model, estimator = estimator)
-      },
-      error = function(e) {
-        NULL
-      }
-    )
+    out$AICc <- tryCatch(performance_aicc(model, estimator = estimator),
+                         error = function(e) NULL)
   }
 
   if ("BIC" %in% toupper(metrics)) {
