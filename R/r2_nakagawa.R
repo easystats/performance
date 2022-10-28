a90e297a0f87281d70521cee8de6908dd8c91385
Daniel <mail@danielluedecke.de>
2022-10-28 08:25:35 +0200

    markdown in docs

--- R/r2_nakagawa.R
+++ R/r2_nakagawa.R
@@ -1,55 +1,49 @@
 #' @title Nakagawa's R2 for mixed models
 #' @name r2_nakagawa
 #'
-#' @description Compute the \emph{marginal} and \emph{conditional} r-squared value for
+#' @description Compute the _marginal_ and _conditional_ r-squared value for
 #'  mixed effects models with complex random effects structures.
 #'
 #' @param model A mixed effects model.
 #' @param by_group Logical, if `TRUE`, returns the explained variance
 #'   at different levels (if there are multiple levels). This is essentially
-#'   similar to the variance reduction approach by \cite{Hox (2010), pp. 69-78}.
+#'   similar to the variance reduction approach by _Hox (2010), pp. 69-78_.
 #' @param tolerance Tolerance for singularity check of random effects, to decide
 #'   whether to compute random effect variances for the conditional r-squared
 #'   or not. Indicates up to which value the convergence result is accepted. When
 #'   `r2_nakagawa()` returns a warning, stating that random effect variances
 #'   can't be computed (and thus, the conditional r-squared is `NA`),
-#'   decrease the tolerance-level. See also [check_singularity()].
+#'   decrease the tolerance-level. See also [`check_singularity()`].
 #'
 #' @return A list with the conditional and marginal R2 values.
 #'
 #' @details
-#'
 #' Marginal and conditional r-squared values for mixed models are calculated
-#' based on \cite{Nakagawa et al. (2017)}. For more details on the computation of
+#' based on _Nakagawa et al. (2017)_. For more details on the computation of
 #' the variances, see `?insight::get_variance`. The random effect variances are
 #' actually the mean random effect variances, thus the r-squared value is also
 #' appropriate for mixed models with random slopes or nested random effects
-#' (see \cite{Johnson, 2014}).
+#' (see _Johnson, 2014_).
 #'
-#' \itemize{
-#'  \item **Conditional R2**: takes both the fixed and random effects into account.
-#'  \item **Marginal R2**: considers only the variance of the fixed effects.
-#' }
+#' - **Conditional R2**: takes both the fixed and random effects into account.
+#' - **Marginal R2**: considers only the variance of the fixed effects.
+#'
 #' The contribution of random effects can be deduced by subtracting the
-#' marginal R2 from the conditional R2 or by computing the [icc()].
+#' marginal R2 from the conditional R2 or by computing the [`icc()`].
 #'
-#' @references \itemize{
-#'  \item Hox, J. J. (2010). Multilevel analysis: techniques and applications
-#'  (2nd ed). New York: Routledge.
-#'
-#'  \item Johnson, P. C. D. (2014). Extension of Nakagawa and Schielzeth’s R2 GLMM
-#'  to random slopes models. Methods in Ecology and Evolution, 5(9), 944–946.
-#'  \doi{10.1111/2041-210X.12225}
-#'
-#'  \item Nakagawa, S., and Schielzeth, H. (2013). A general and simple method for
-#'  obtaining R2 from generalized linear mixed-effects models. Methods in
-#'  Ecology and Evolution, 4(2), 133–142. \doi{10.1111/j.2041-210x.2012.00261.x}
-#'
-#'  \item Nakagawa, S., Johnson, P. C. D., and Schielzeth, H. (2017). The
-#'  coefficient of determination R2 and intra-class correlation coefficient from
-#'  generalized linear mixed-effects models revisited and expanded. Journal of
-#'  The Royal Society Interface, 14(134), 20170213. \doi{10.1098/rsif.2017.0213}
-#'  }
+#' @references
+#'  - Hox, J. J. (2010). Multilevel analysis: techniques and applications
+#'    (2nd ed). New York: Routledge.
+#'  - Johnson, P. C. D. (2014). Extension of Nakagawa and Schielzeth’s R2 GLMM
+#'    to random slopes models. Methods in Ecology and Evolution, 5(9), 944–946.
+#'    \doi{10.1111/2041-210X.12225}
+#'  - Nakagawa, S., and Schielzeth, H. (2013). A general and simple method for
+#'    obtaining R2 from generalized linear mixed-effects models. Methods in
+#'    Ecology and Evolution, 4(2), 133–142. \doi{10.1111/j.2041-210x.2012.00261.x}
+#'  - Nakagawa, S., Johnson, P. C. D., and Schielzeth, H. (2017). The
+#'    coefficient of determination R2 and intra-class correlation coefficient from
+#'    generalized linear mixed-effects models revisited and expanded. Journal of
+#'    The Royal Society Interface, 14(134), 20170213. \doi{10.1098/rsif.2017.0213}
 #'
 #' @examples
 #' if (require("lme4")) {
