a4d81173ea839ee710bca266811a0d3347c8ac64
Daniel <mail@danielluedecke.de>
2022-12-12 19:21:01 +0100

    fix

--- tests/testthat/test-performance_aic.R
+++ tests/testthat/test-performance_aic.R
@@ -1,9 +1,10 @@
 if (requiet("testthat") && requiet("performance")) {
   data("mtcars")
   mtcars$mpg <- floor(mtcars$mpg)
+  d <<- mtcars
 
-  model_lnorm <- lm(log(mpg) ~ factor(cyl), mtcars)
-  model_norm <- lm(mpg ~ factor(cyl), mtcars)
+  model_lnorm <- lm(log(mpg) ~ factor(cyl), d)
+  model_norm <- lm(mpg ~ factor(cyl), dd)
   model_pois <- glm(mpg ~ factor(cyl), mtcars, family = poisson())
 
   test_that("performance_aic works", {
