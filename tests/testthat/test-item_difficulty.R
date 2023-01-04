cdde05f81f62149d8564e317d390e9a772f20afc
Daniel <mail@danielluedecke.de>
2023-01-04 13:54:13 +0100

    add test

--- tests/testthat/test-item_difficulty.R
+++ tests/testthat/test-item_difficulty.R
@@ -18,3 +18,13 @@ test_that("item_difficulty", {
     tolerance = 1e-3
   )
 })
+
+test_that("item_difficulty, maximum value", {
+  x <- data.frame(a = rep(NA, 5), b = 1:5, c = c(1:4, 6))
+  out1 <- item_difficulty(x)
+  out2 <- item_difficulty(x, maximum_value = 6)
+  expect_equal(out1, out2, tolerance = 1e-3, ignore_attr = TRUE)
+
+  out <- item_difficulty(x, maximum_value = 10)
+  expect_equal(out$difficulty, c(NaN, 0.3, 0.32), tolerance = 1e-3, ignore_attr = TRUE)
+})
