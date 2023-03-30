d8fc61c1891b1103e078f093c61f88fcf818ef6c
Daniel <mail@danielluedecke.de>
2023-03-30 21:05:17 +0200

    fix

--- tests/testthat/test-test_wald.R
+++ tests/testthat/test-test_wald.R
@@ -12,8 +12,10 @@ test_that("test_wald - lm", {
   expect_equal(rez$p, ref$`Pr(>F)`, tolerance = 1e-4)
 
   # setting test = "LRT" is not expected, but should at least not fail
-  expect_warning(rez <- test_wald(m1, m2, test = "LRT"))
-  expect_equal(rez$Name, c("..1", "..2"))
+  expect_message({
+    rez <- test_wald(m1, m2, test = "LRT")
+  })
+  expect_identical(rez$Name, c("..1", "..2"))
 
   # Reversed
   m3 <- m1
@@ -31,7 +33,9 @@ test_that("test_wald - glm", {
   m2 <- glm(vs ~ disp + hp, data = mtcars, family = "binomial")
   m3 <- glm(vs ~ disp, data = mtcars, family = "binomial")
 
-  expect_warning(rez <- test_wald(m1, m2, m3))
+  expect_message({
+    rez <- test_wald(m1, m2, m3)
+  })
   ref <- as.data.frame(anova(m1, m2, m3, test = "LRT"))
 
   expect_equal(rez$Chi2, abs(ref$Deviance), tolerance = 1e-4)
