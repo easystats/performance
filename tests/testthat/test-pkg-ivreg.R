a571644d69e0388e88dc3cfb27b137bde234e0fb
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-03-31 21:04:32 +0200

    fix styling

--- tests/testthat/test-pkg-ivreg.R
+++ tests/testthat/test-pkg-ivreg.R
@@ -1,7 +1,7 @@
 test_that("Issue #530 from the `modelsummary` repo", {
   skip_if_not_installed("ivreg")
   skip_if_not(packageVersion("insight") >= "0.19.1.3") # formatting of results
-                                                      # for ivreg diagnostics
+  # for ivreg diagnostics
   data(mtcars)
   iv_model <- suppressMessages(ivreg::ivreg(mpg ~ qsec + cyl + drat | disp | wt, data = mtcars))
   out <- expect_silent(model_performance(iv_model))
