5e02cd56ddd87a455dca97d6f85f9c26660155af
Daniel <mail@danielluedecke.de>
2023-04-06 15:16:53 +0200

    require min version

--- tests/testthat/test-cronbachs_alpha.R
+++ tests/testthat/test-cronbachs_alpha.R
@@ -9,13 +9,14 @@ test_that("cronbachs_alpha", {
 
 
 test_that("cronbachs_alpha, principal_components", {
-  skip_if_not_installed("parameters")
+  skip_if_not_installed("parameters", minimum_version = "0.20.3")
   pca <- parameters::principal_components(mtcars[, c("cyl", "gear", "carb", "hp")], n = 1)
   expect_equal(cronbachs_alpha(pca, verbose = FALSE), c(PC1 = 0.1101384), tolerance = 1e-3)
   expect_warning(cronbachs_alpha(pca))
 })
+
 test_that("cronbachs_alpha, principal_components", {
-  skip_if_not_installed("parameters")
+  skip_if_not_installed("parameters", minimum_version = "0.20.3")
   pca <- parameters::principal_components(mtcars, n = 2)
   expect_equal(cronbachs_alpha(pca), c(PC1 = 0.4396, PC2 = -1.44331), tolerance = 1e-3)
 })
