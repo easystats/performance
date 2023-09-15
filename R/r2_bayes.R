6c95f678d7034807453676595c710fe85942dc3c
Daniel <mail@danielluedecke.de>
2023-09-15 12:14:52 +0200

    try to reduce warnings in examples and use examplesIf

--- R/r2_bayes.R
+++ R/r2_bayes.R
@@ -43,13 +43,13 @@
 #'   ))
 #'   r2_bayes(model)
 #'
-#'   model <- stan_lmer(
+#'   model <- suppressWarnings(stan_lmer(
 #'     Petal.Length ~ Petal.Width + (1 | Species),
 #'     data = iris,
 #'     chains = 1,
 #'     iter = 500,
 #'     refresh = 0
-#'   )
+#'   ))
 #'   r2_bayes(model)
 #' }
 #'
