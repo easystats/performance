f6c86b817eb6af54c87443fdebad6e3ee083fe04
Daniel <mail@danielluedecke.de>
2024-03-04 21:21:07 +0100

    test

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -4,8 +4,10 @@ withr::with_options(
   test_that(".safe works with options", {
     expect_error(performance:::.safe(mean(fd)), regex = "object 'fd' not found")
     expect_identical(performance:::.safe(mean(fd), 1L), 1L)
+    expect_identical(performance:::.safe(mean(c(1, 2, 3))), 2)
   })
 )
 test_that(".safe works", {
   expect_null(performance:::.safe(mean(fd)))
+  expect_identical(performance:::.safe(mean(c(1, 2, 3))), 2)
 })
