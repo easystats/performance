c0940251fae0efc9add5ac1e0e7e8102ddd92343
Daniel <mail@danielluedecke.de>
2024-03-17 11:43:50 +0100

    fix test

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -1,21 +1,21 @@
 test_that("check_factorstructure", {
   skip_if_not_installed("parameters")
   x <- check_factorstructure(mtcars)
-  expect_equal(x$KMO$MSA, 0.826, tolerance = 0.01)
-  expect_equal(x$sphericity$chisq, 408.011, tolerance = 0.01)
+  expect_equal(x$KMO$MSA, 0.8265536, tolerance = 0.01)
+  expect_equal(x$sphericity$chisq, 408.0116, tolerance = 0.01)
 })
 
 test_that("check_clusterstructure, ok", {
   skip_if_not_installed("parameters")
   set.seed(333)
   out <- check_clusterstructure(iris[, 1:4])
-  expect_equal(out$H, 0.187, tolerance = 0.01)
+  expect_equal(out$H, 0.1869618, tolerance = 0.01)
   expect_identical(
     capture.output(print(out)),
     c(
       "# Clustering tendency",
       "",
-      "The dataset is suitable for clustering (Hopkins' H = 0.18)."
+      "The dataset is suitable for clustering (Hopkins' H = 0.19)."
     )
   )
 })
