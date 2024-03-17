567f2c1abfa15f7b99530c8b7cae47e0debac031
Daniel <mail@danielluedecke.de>
2024-03-17 11:25:43 +0100

    add tests

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -5,8 +5,32 @@ test_that("check_factorstructure", {
   expect_equal(x$sphericity$chisq, 408.011, tolerance = 0.01)
 })
 
-test_that("check_clusterstructure", {
+test_that("check_clusterstructure, ok", {
   skip_if_not_installed("parameters")
   set.seed(333)
-  expect_equal(check_clusterstructure(iris[, 1:4])$H, 0.187, tolerance = 0.01)
+  out <- check_clusterstructure(iris[, 1:4])
+  expect_equal(out$H, 0.187, tolerance = 0.01)
+  expect_identical(
+    capture.output(print(out)),
+    c(
+      "# Clustering tendency",
+      "",
+      "The dataset is suitable for clustering (Hopkins' H = 0.18)."
+    )
+  )
+})
+
+test_that("check_clusterstructure, bad", {
+  skip_if_not_installed("parameters")
+  set.seed(13)
+  out <- check_clusterstructure(mtcars[, 10:11])
+  expect_equal(out$H, 0.5142575, tolerance = 0.01)
+  expect_identical(
+    capture.output(print(out)),
+    c(
+      "# Clustering tendency",
+      "",
+      "The dataset is not suitable for clustering (Hopkins' H = 0.51)."
+    )
+  )
 })
