4491b247353f67146f814eeb219e9e331cbdab1d
Daniel <mail@danielluedecke.de>
2024-04-03 11:37:07 +0200

    style

--- tests/testthat/test-cronbachs_alpha.R
+++ tests/testthat/test-cronbachs_alpha.R
@@ -3,10 +3,10 @@ test_that("cronbachs_alpha, data frame", {
   x <- mtcars[, c("cyl", "gear", "carb", "hp")]
   expect_equal(cronbachs_alpha(x), 0.09463206, tolerance = 1e-3)
 })
+
 test_that("cronbachs_alpha", {
   expect_message(expect_null(cronbachs_alpha(mtcars[1])), regex = "Too few")
 })
-
 
 test_that("cronbachs_alpha, principal_components", {
   skip_if_not_installed("parameters", minimum_version = "0.21.3")
@@ -24,7 +24,6 @@ test_that("cronbachs_alpha, principal_components", {
   pca <- parameters::principal_components(mtcars, n = 2)
   expect_equal(cronbachs_alpha(pca), c(PC1 = 0.4396, PC2 = -1.44331), tolerance = 1e-3)
 })
-
 
 test_that("cronbachs_alpha, matrix", {
   m <- as.matrix(mtcars[c("cyl", "gear", "carb", "hp")])
