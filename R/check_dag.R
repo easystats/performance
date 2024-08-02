c8f8e7592495ced41548f3ff24b814a8a234f466
Daniel <mail@danielluedecke.de>
2024-08-03 01:52:45 +0200

    more

--- R/check_dag.R
+++ R/check_dag.R
@@ -14,6 +14,8 @@ check_dag <- function(...,
     reason = "to check correct adjustments for identifying causal effects."
   )
 
+  effect <- match.arg(effect)
+
   # retrieve formulas
   formulas <- list(...)
 
@@ -46,7 +48,8 @@ check_dag <- function(...,
   }
 
   # convert to dag
-  dag <- ggdag::dagify(formulas, exposure = exposure, outcome = outcome)
+  dag_args <- c(formulas, list(exposure = exposure, outcome = outcome))
+  dag <- do.call(ggdag::dagify, dag_args)
 
   # add adjustments
   if (!is.null(adjusted)) {
@@ -55,7 +58,24 @@ check_dag <- function(...,
     }
   }
 
+  # data for checking effects
+  checks <- lapply(c("direct", "total"), function(x) {
+    adjustment_set <- unlist(dagitty::adjustmentSets(dag, effect = x), use.names = FALSE)
+    adjustment_nodes <- unlist(dagitty::adjustedNodes(dagitty::adjustmentSets(dag, effect = x)), use.names = FALSE)
+    list(
+      adjustment_not_needed = is.null(adjustment_set) && is.null(adjustment_nodes),
+      incorrectly_adjusted = is.null(adjustment_set) && !is.null(adjustment_nodes),
+      current_adjustments = adjustment_nodes,
+      minimal_adjustments = adjustment_set
+    )
+  })
+
   attr(dag, "effect") <- effect
+  attr(dag, "outcome") <- outcome
+  attr(dag, "exposure") <- exposure
+  attr(dag, "check_direct") <- insight::compact_list(checks[[1]])
+  attr(dag, "check_total") <- insight::compact_list(checks[[2]])
+
   class(dag) <- c("check_dag", class(dag))
 
   dag
@@ -63,25 +83,67 @@ check_dag <- function(...,
 
 
 #' @export
-print.check_dag <- function(x, effect = NULL, ...) {
-  if (is.null(effect)) {
-    effect <- attributes(x)$effect
+print.check_dag <- function(x, ...) {
+  insight::check_if_installed("dagitty")
+  effect <- attributes(x)$effect
+
+  for (i in c("direct", "total")) {
+    if (i == "direct") {
+      out <- attributes(x)$check_direct
+    } else {
+      out <- attributes(x)$check_total
+    }
+
+    # build message with check results for effects -----------------------
+
+    if (isTRUE(out$adjustment_not_needed)) {
+      # Scenario 1: no adjustment needed
+      msg <- paste0(
+        "Model is correctly specified. No adjustment is necessary for estimating the ", i, " effect of ",
+        datawizard::text_concatenate(attributes(x)$exposure),
+        " on ",
+        attributes(x)$outcome,
+        "."
+      )
+    } else if (out$incorrectly_adjusted) {
+      # Scenario 2: incorrectly adjusted, adjustments where none is allowed
+      msg <- paste0(
+        "Incorrectly adjusted! To estimate the ", i, " effect of ",
+        datawizard::text_concatenate(attributes(x)$exposure),
+        " on ",
+        attributes(x)$outcome,
+        ", do *not* adjust for following variables: ",
+        datawizard::text_concatenate(out$current_adjustments),
+        "."
+      )
+    } else if (length(out$current_adjustments) != length(out$minimal_adjustment)) {
+      # Scenario 3: missing adjustments
+      msg <- paste0(
+        "Incorrectly adjusted! The minimal sufficient adjustments for estimating the ", i, " effect of ",
+        datawizard::text_concatenate(attributes(x)$exposure),
+        " on ",
+        attributes(x)$outcome,
+        " are following variables: ",
+        datawizard::text_concatenate(out$minimal_adjustments),
+        ". However, the model currently only adjusts for ",
+        datawizard::text_concatenate(out$current_adjustments),
+        "."
+      )
+    } else {
+      # Scenario 4: correct adjustment
+      msg <- paste0(
+        "Model is correctly specified. All minimal sufficient adjustments for estimating the ", i, " effect of ",
+        datawizard::text_concatenate(attributes(x)$exposure),
+        " on ",
+        attributes(x)$outcome,
+        " were done."
+      )
+    }
+
+    if (effect %in% c("all", i)) {
+      cat(insight::print_color(insight::format_message(paste0("# Correct adjustments for identifying {.i ", i, "} effects\n\n")), "blue"))
+      cat(insight::format_message(msg))
+      cat("\n\n")
+    }
   }
-
-  if (effect)
-  dagitty::adjustmentSets(dag)
-  ggdag_adjustment_set(dag)
-  # wofür müssen wir adjustieren, wenn wir am direkten Effekt interessiert sind?
-  dagitty::adjustmentSets(dag, effect = "direct")
-  ggdag_adjustment_set(dag, effect = "direct")
-  # incorrect adjustement - adjustmentSets() liefert leeres Element und
-  # adjustedNodes() hat mehr als 1 Element
-  r <- dagitty::adjustmentSets(dag)
-  incorrectly_adjusted <- (!length(r) && length(dagitty::adjustedNodes(dag)) > 0)
-  incorrectly_adjusted
-  r <- dagitty::adjustmentSets(dag, effect = "direct")
-  incorrectly_adjusted <- (!length(r) && length(dagitty::adjustedNodes(dag)) > 0)
-  incorrectly_adjusted
-
-
 }
