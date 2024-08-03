dbb1cc3bdb992c3ee77447b804b2fdfbc9463da7
Daniel <mail@danielluedecke.de>
2024-08-03 18:31:34 +0200

    docs, move plot to see

--- R/check_dag.R
+++ R/check_dag.R
@@ -7,7 +7,8 @@
 #' `ggdag::dagify()`, which used `dagitty::adjustmentSets()` and
 #' `dagitty::adjustedNodes()` to check if the model is correctly adjusted for
 #' identifying causal (direct and total) effects of a given exposure on the
-#' outcome.
+#' outcome. `as.dag()` is a small convenient function to return the dagitty-string,
+#' which can be used for the online-tool from the dagitty-website.
 #'
 #' @param ... One or more formulas, which are converted into **dagitty** syntax.
 #' First element may also be model object. If a model objects is provided, its
@@ -266,81 +267,5 @@ print.check_dag <- function(x, ...) {
       cat(msg)
       cat("\n\n")
     }
-  }
-}
-
-
-#' @export
-plot.check_dag <- function(x, size_point = 15, colors = NULL, which = "all", ...) {
-  .data <- NULL
-  insight::check_if_installed(c("ggdag", "ggplot2", "see"))
-  which <- match.arg(which, choices = c("all", "current", "required"))
-
-  p1 <- suppressWarnings(ggdag::ggdag_adjust(x, stylized = TRUE))
-  p2 <- suppressWarnings(ggdag::ggdag_adjustment_set(x, shadow = TRUE, stylized = TRUE))
-
-  # tweak data
-  p1$data$type <- as.character(p1$data$adjusted)
-  p1$data$type[vapply(p1$data$name, ggdag::is_collider, logical(1), .dag = x)] <- "collider"
-  p1$data$type[p1$data$name == attributes(x)$outcome] <- "outcome"
-  p1$data$type[p1$data$name %in% attributes(x)$exposure] <- "exposure"
-  p1$data$type <- factor(p1$data$type, levels = c("outcome", "exposure", "adjusted", "unadjusted", "collider"))
-
-  p2$data$type <- as.character(p2$data$adjusted)
-  p2$data$type[vapply(p2$data$name, ggdag::is_collider, logical(1), .dag = x)] <- "collider"
-  p2$data$type[p2$data$name == attributes(x)$outcome] <- "outcome"
-  p2$data$type[p2$data$name %in% attributes(x)$exposure] <- "exposure"
-  p2$data$type <- factor(p2$data$type, levels = c("outcome", "exposure", "adjusted", "unadjusted", "collider"))
-
-  if (is.null(colors)) {
-    point_colors <- see::see_colors(c("yellow", "cyan", "blue grey", "red", "orange"))
-  } else if (length(colors) != 5) {
-    insight::format_error("`colors` must be a character vector with five color-values.")
-  } else {
-    point_colors <- colors
-  }
-  names(point_colors) <- c("outcome", "exposure", "adjusted", "unadjusted", "collider")
-
-  plot1 <- ggplot2::ggplot(p1$data, ggplot2::aes(x = .data$x, y = .data$y)) +
-    see::geom_point_borderless(ggplot2::aes(fill = .data$type), size = size_point) +
-    ggdag::geom_dag_edges(
-      ggplot2::aes(
-        xend = .data$xend,
-        yend = .data$yend,
-        edge_alpha = .data$adjusted
-      )
-    ) +
-    ggdag::scale_adjusted() +
-    ggdag::geom_dag_label(ggplot2::aes(label = .data$name)) +
-    ggdag::theme_dag() +
-    ggplot2::scale_fill_manual(values = point_colors) +
-    ggplot2::ggtitle("Current model") +
-    ggplot2::guides(edge_alpha = "none")
-
-  plot2 <- ggplot2::ggplot(p2$data, ggplot2::aes(x = .data$x, y = .data$y)) +
-    see::geom_point_borderless(ggplot2::aes(fill = .data$type), size = size_point) +
-    ggdag::geom_dag_edges(
-      ggplot2::aes(
-        xend = .data$xend,
-        yend = .data$yend,
-        edge_alpha = .data$adjusted
-      )
-    ) +
-    ggdag::scale_adjusted() +
-    ggdag::geom_dag_label(ggplot2::aes(label = .data$name)) +
-    ggdag::theme_dag() +
-    ggplot2::scale_fill_manual(values = point_colors) +
-    ggplot2::ggtitle("Required model") +
-    ggplot2::guides(edge_alpha = "none")
-
-  if (which == "all") {
-    # fix legends
-    plot2 <- plot2 + ggplot2::theme(legend.position = "none")
-    # plot
-    see::plots(plot1, plot2, n_rows = 1)
-  } else if (which == "current") {
-    plot1
-  } else {
-    plot2
   }
 }
