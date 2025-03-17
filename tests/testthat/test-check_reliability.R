d7cc373430e10cbd0f3432dd23ba92254fa899d4
Daniel <mail@danielluedecke.de>
2025-03-17 15:53:07 +0100

    update test

--- tests/testthat/test-check_reliability.R
+++ tests/testthat/test-check_reliability.R
@@ -7,23 +7,20 @@ test_that("check_reliability - frequentist", {
 
   m <- lme4::lmer(Sepal.Width ~ Petal.Width + (Petal.Width | Place), data = data)
   out <- check_reliability(m)
-  testthat::expect_true(all(dim(out) == c(2, 5)))
+  expect_identical(dim(out), c(2L, 5L))
 
   m <- glmmTMB::glmmTMB(Sepal.Width ~ Petal.Width + (Petal.Width | Place), data = data)
   out <- check_reliability(m)
-  testthat::expect_true(all(dim(out) == c(2, 5)))
+  expect_identical(dim(out), c(2L, 5L))
 })
 
 test_that("check_reliability - Bayesian", {
-  skip_if_offline()
   skip_if_not_installed("curl")
+  skip_if_offline()
   skip_if_not_installed("httr2")
   skip_if_not_installed("rstanarm")
   skip_if_not_installed("rstantools")
   skip_if_not_installed("brms")
-
-  library(rstanarm)
-
 
   # m <- rstanarm::stan_lmer(
   #   Sepal.Width ~ Petal.Width + (Petal.Width | Group),
@@ -37,10 +34,12 @@ test_that("check_reliability - Bayesian", {
   #   backend="cmdstanr"
   # )
   m <- insight::download_model("brms_mixed_10")
+  skip_if(is.null(m))
   out <- check_reliability(m)
-  testthat::expect_true(all(dim(out) == c(2, 5)))
+  expect_identical(dim(out), c(2L, 5L))
 
   m <- insight::download_model("brms_sigma_3")
+  skip_if(is.null(m))
   out <- check_reliability(m)
-  testthat::expect_true(all(dim(out) == c(4, 6)))
+  expect_identical(dim(out), c(4L, 6L))
 })
