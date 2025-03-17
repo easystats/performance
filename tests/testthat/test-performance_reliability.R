a1e94f7c2bfb64036952b139ae5d9cb64ceb247a
Daniel <mail@danielluedecke.de>
2025-03-17 15:54:34 +0100

    rename

--- tests/testthat/test-performance_reliability.R
+++ tests/testthat/test-performance_reliability.R
@@ -1,4 +1,4 @@
-test_that("check_reliability - frequentist", {
+test_that("performance_reliability - frequentist", {
   skip_if_not_installed("lme4")
   skip_if_not_installed("glmmTMB")
 
@@ -6,15 +6,15 @@ test_that("check_reliability - frequentist", {
   data$Place <- as.factor(rep(c("P1", "P2", "P3", "P4", "P5", "P6"), each = 25))
 
   m <- lme4::lmer(Sepal.Width ~ Petal.Width + (Petal.Width | Place), data = data)
-  out <- check_reliability(m)
+  out <- performance_reliability(m)
   expect_identical(dim(out), c(2L, 5L))
 
   m <- glmmTMB::glmmTMB(Sepal.Width ~ Petal.Width + (Petal.Width | Place), data = data)
-  out <- check_reliability(m)
+  out <- performance_reliability(m)
   expect_identical(dim(out), c(2L, 5L))
 })
 
-test_that("check_reliability - Bayesian", {
+test_that("performance_reliability - Bayesian", {
   skip_if_not_installed("curl")
   skip_if_offline()
   skip_if_not_installed("httr2")
@@ -33,11 +33,11 @@ test_that("check_reliability - Bayesian", {
   # )
   m <- insight::download_model("brms_mixed_10")
   skip_if(is.null(m))
-  out <- check_reliability(m)
+  out <- performance_reliability(m)
   expect_identical(dim(out), c(2L, 5L))
 
   m <- insight::download_model("brms_sigma_3")
   skip_if(is.null(m))
-  out <- check_reliability(m)
+  out <- performance_reliability(m)
   expect_identical(dim(out), c(4L, 6L))
 })
