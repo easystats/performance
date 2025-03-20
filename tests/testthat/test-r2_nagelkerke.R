cfd5d026bcfa27f9561b2d77f138d68011615116
Daniel <mail@danielluedecke.de>
2025-03-21 00:57:12 +0100

    styler

--- tests/testthat/test-r2_nagelkerke.R
+++ tests/testthat/test-r2_nagelkerke.R
@@ -23,21 +23,21 @@ test_that("r2_nagelkerke, multinom, correct base-model with NA", {
   skip_if_not_installed("nnet")
 
   n_obs <- 1000
-  softmax <- function(x){
+  softmax <- function(x) {
     exp(x - max(x)) / sum(exp(x - max(x)))
   }
-  sample_y <- function(x){
+  sample_y <- function(x) {
     sample(1:3, size = 1, prob = softmax(c(0.25 * x, -0.1 * x, 0 * x)))
   }
   set.seed(123)
   sim_df <- data.frame(x = rnorm(n_obs, 0, 1), y = NA)
 
-  for(i in 1:nrow(sim_df)){
+  for (i in 1:nrow(sim_df)) {
     sim_df$y[i] <- sample_y(sim_df$x[i])
   }
 
   sim_df$x[1:500] <- NA
-  sim_df2 <- sim_df[!is.na(sim_df$x),]
+  sim_df2 <- sim_df[!is.na(sim_df$x), ]
 
   m1 <- nnet::multinom(y ~ x, data = sim_df, trace = FALSE)
   m2 <- nnet::multinom(y ~ x, data = sim_df2, trace = FALSE)
