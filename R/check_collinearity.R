eb40ea1dbc6098b3a3b0beb926a31319543fad6b
Ben Bolker <bbolker@gmail.com>
2025-10-14 14:59:34 -0400

    switch from URL to DOI for Vanhove collinearity paper

--- R/check_collinearity.R
+++ R/check_collinearity.R
@@ -124,7 +124,7 @@
 #'   examples in R and Stan. 2nd edition. Chapman and Hall/CRC.
 #'
 #' - Vanhove, J. (2021) Collinearity Isn’t a Disease That Needs Curing.
-#'   Meta-Psychology, 5. \url{https://doi.org/10.15626/MP.2021.2548}
+#'   Meta-Psychology, 5. \doi{10.15626/MP.2021.2548}
 #'
 #' - Zuur AF, Ieno EN, Elphick CS. A protocol for data exploration to avoid
 #'   common statistical problems: Data exploration. Methods in Ecology and
