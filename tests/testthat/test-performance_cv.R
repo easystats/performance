0c931047f358be59e7d22299f22f49dce2530f6f
Daniel <mail@danielluedecke.de>
2026-05-20 17:53:50 +0200

    performance_cv() fails with method = "loo" (object ‘test_resd’ not found) Fixes #896

--- tests/testthat/test-performance_cv.R
+++ tests/testthat/test-performance_cv.R
@@ -1,17 +1,22 @@
-model <- lm(mpg ~ wt + cyl, data = mtcars)
-
-set.seed(123)
-out <- performance_cv(model)
-
 test_that("performance_cv", {
-  expect_equal(out$MSE, 5.91153, tolerance = 1e-3)
-  expect_equal(colnames(out), c("MSE", "RMSE", "R2"), tolerance = 1e-3)
-})
+  data(mtcars)
+  model <- lm(mpg ~ wt + cyl, data = mtcars)
 
-set.seed(123)
-out <- performance_cv(model, metrics = c("MSE", "R2"))
-
-test_that("performance_cv", {
+  set.seed(123)
+  out <- performance_cv(model)
   expect_equal(out$MSE, 5.91153, tolerance = 1e-3)
-  expect_equal(colnames(out), c("MSE", "R2"), tolerance = 1e-3)
+  expect_named(out, c("MSE", "RMSE", "R2"))
+
+  set.seed(123)
+  out <- performance_cv(model, metrics = c("MSE", "R2"))
+  expect_equal(out$MSE, 5.91153, tolerance = 1e-3)
+  expect_named(out, c("MSE", "R2"))
+
+  set.seed(123)
+  out <- performance_cv(model, method = "loo")
+  expect_equal(out$MSE, 7.376451, tolerance = 1e-3)
+
+  set.seed(123)
+  out <- performance_cv(model, method = "k_fold")
+  expect_equal(out$MSE, 9.65578, tolerance = 1e-3)
 })
